e9a705a5b55d67b5903bd80ebadf5376e832254e
Brian Hardenstein <pixelatedbrian@gmail.com>
2018-04-05 20:22:40 -0700

    made a generator for loading images into inception v3, created splitter.py and loader_bot.py which contain helper functions and classes

---
+++ notebooks/loader_bot.py
@@ -0,0 +1,66 @@
+import numpy as np
+import keras
+import cv2
+
+
+class LoaderBot(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, batch_size=32, dim=(299, 299), n_channels=3,
+                 n_classes=128, shuffle=False):
+        '''
+        Initialization
+        Already shuffled by get_skfold_indicies so no need to shuffle again
+        '''
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size), dtype=int)
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp):
+            # Store sample
+            # X[i,] = np.load(ID)
+
+            X[i,] = cv2.imread(ID)
+
+            # ID:
+            # ../data/stage1_imgs/flip_116297_85.jpg
+
+            # Store class
+            # y[i] = self.labels[ID]
+            y[i] = int(ID.split("/")[-1].split(".")[0].split("_")[-1]) - 1
+
+        return X, keras.utils.to_categorical(y, num_classes=self.n_classes)
